ba307f95d085b67079398e4a9cbb9411da773239
Mohammed <mrcloud.med@gmail.com>
2023-10-05 17:39:43 -0700

    C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char charType;
+int intType;
+long int longintType;
+long float longfloatType;
+float floatType;
+printf("Size of a char: %zu byte(s),sizeof(charType));
+printf("Size of an int: %zu byte(s),sizeof(intType));
+printf("Size of a long int: %zu byte(s),sizeof(longintType));
+printf("Size of a long long int: %zu byte(s),sizeof(longfloatType));
+printf("Size of a float: %zu byte(s),sizeof(floatType));
+return (0)
+}
