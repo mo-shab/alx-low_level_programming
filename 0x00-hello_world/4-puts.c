bea3819c6750868cd382989dd07244c291d60a37
Mohammed <mrcloud.med@gmail.com>
2023-10-05 17:20:45 -0700

    ./a.out

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
  * Description: 'This Program output a string without function printf'
  * @parameter: describe the parameter
- * 
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char *str = "\"Programming is like building a multilingual puzzle";
 
-	puts(str);	
+	puts(str);
 	return (0);
 }
