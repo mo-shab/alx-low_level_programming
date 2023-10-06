f1fc303963faeb8ae3f01609ec3d3d0c6c182d27
Mohammed <mrcloud.med@gmail.com>
2023-10-05 18:05:24 -0700

    Quote C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,9 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void){
+int main(void)
+{
 const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, message, 58);
+write(2, message, 59);
 return (1);
 }
