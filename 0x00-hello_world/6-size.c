cdc5ad4f72cc62b640b0a1eb5e6f3e35bf21553d
Mohammed <mrcloud.med@gmail.com>
2023-10-05 17:45:20 -0700

    C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int intType;
 long int longintType;
 double longfloatType;
 float floatType;
-printf("Size of a char: %zu byte(s)\n",sizeof(charType));
-printf("Size of an int: %zu byte(s)\n",sizeof(intType));
-printf("Size of a long int: %zu byte(s)\n",sizeof(longintType));
-printf("Size of a long long int: %zu byte(s)\n",sizeof(longfloatType));
-printf("Size of a float: %zu byte(s)\n",sizeof(floatType));
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(longfloatType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }
