b48ab908b1bf35cfa0c3d56aa5bb1933ed481dae
Mohammed <mrcloud.med@gmail.com>
2023-10-05 17:26:42 -0700

    Correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: 'This Program output a string without function printf'
- * @parameter: describe the parameter
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char *str = "\"Programming is like building a multilingual puzzle";
-
-	puts(str);
+	puts( "\"Programming is like building a multilingual puzzle");
 	return (0);
 }
