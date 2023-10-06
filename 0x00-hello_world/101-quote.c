bfa281137137e5738477ae699653fb346f0387ca
Mohammed <mrcloud.med@gmail.com>
2023-10-05 18:02:41 -0700

    Quote C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <unistd.h>
-/*
-This a new function
-*/
-int main (void){
-const char *message="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2,message,55);
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void){
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, message, 58);
 return (1);
 }
