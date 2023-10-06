ab3faf567a847154d17618f09919065e958611d7
Mohammed <mrcloud.med@gmail.com>
2023-10-07 00:23:11 +0100

    third program

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,11 +6,10 @@
  */
 int main(void)
 {
-    char ch;
-
-    for (ch='a'; ch<= 'z'; ch++)
-    {
-        putchar(ch);
-    }
-    return (0);
+char ch;
+for (ch='a'; ch<= 'z'; ch++)
+{
+putchar(ch);
+}
+return (0);
 }
