d51d2426f5e0c6de480d0941c6a9ccb843769482
Mohammed <mrcloud.med@gmail.com>
2023-10-07 01:33:05 +0100

    fourth program

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,7 +18,9 @@ putchar((i % 10) + '0');
 putchar((j % 10) + '0');
 putchar((k % 10) + '0');
 if (i == 7 && j == 8 && k == 9)
-continue;		
+{
+continue;
+}
 putchar(',');
 putchar(' ');
 }
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,2 @@
+Welcome to 0X01 Variable, IF ELSE, While, Do While Programs
 This will have Variable IF ELSE in C
