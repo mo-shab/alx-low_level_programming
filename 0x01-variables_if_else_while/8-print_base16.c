fdbfb8e28e1ae870b16f6dbeb17e58408ec853a4
Mohammed <mrcloud.med@gmail.com>
2023-10-07 01:03:18 +0100

    fourth program

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * This program use putchar to print the base 16
+ * using the code ASCII, we give it to putchar
+ * we are not allowed to use printf or put...
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 58; i++)
+{
+putchar(i);
+}
+for (i = 97; i<= 102; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
