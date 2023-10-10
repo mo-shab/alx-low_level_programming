044533a8910dce0a45fb9dc51f15b7c485e86e74
Mohammed <mrcloud.med@gmail.com>
2023-10-10 03:04:07 -0700

    putchar

---
+++ 0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 0.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void main(void)
+{
+printf("_puthchar\n");
+return 0;
+}
