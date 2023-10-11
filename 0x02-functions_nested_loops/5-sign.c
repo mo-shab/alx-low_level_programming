002142c6419cd3bf60b7967d70f4397aa415741a
Mohammed <mrcloud.med@gmail.com>
2023-10-10 17:43:51 -0700

    porjet print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign: is a function that prints the sign of a number
+ * Return: 1 And print + if n is greater than 0.
+ * 0 and print 0 if n is 0.
+ * -1 and print -1 if n less than 0.
+ * @n: is the num we will test on it.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf('+');
+		return (1);
+	}
+	else
+		if ( n == 0)
+		{
+			printf('0');
+			return (0);
+		} else
+			{
+			printf("-1");
+			return (-1);
+			}
+}
