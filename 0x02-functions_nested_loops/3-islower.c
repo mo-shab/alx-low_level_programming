38c8e53797afb2d3f9109366be4eec643544c87c
Mohammed <mrcloud.med@gmail.com>
2023-10-10 17:06:50 -0700

    projet

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * Prototype : int _isloer (int c);
+ * retunr 1 if c is lowercase
+ * return 0 is otherwise
+ */
+int _islower(int c);
+{
+if( c == 'c')
+{
+return 1;
+}
+return 0;
+}
