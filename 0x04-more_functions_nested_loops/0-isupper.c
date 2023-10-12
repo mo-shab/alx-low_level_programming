84520d5b722c9cea7a377708f9cc4bc35152d608
Mohammed <mrcloud.med@gmail.com>
2023-10-13 01:07:14 +0200

    Correction

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * _isupper(int c) : test if c is an upper
+ * _isupper - test if c is an upper
  * @C: us the variable we gonna test
  * return: 1 if C is upper
  * 0 if C is otherwise
 */
-
 int _isupper(int c)
 {
 if (c >= 65 && c <= 90)
-   {
+{
 return (1);
 }
 
