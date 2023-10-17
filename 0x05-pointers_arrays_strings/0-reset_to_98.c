5577cf014bd48ae35da0551327ea75912e6adf9c
Mohammed <mrcloud.med@gmail.com>
2023-10-17 02:08:45 +0200

    update code

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,11 +4,10 @@
   * void reset_to_98 - point to an int as parameter and update it's value
   * @n: the int as pointer
   *
-  * Return: 0
+  * Return: empty
   */
 
   void reset_to_98(int *n)
   {
     *n = 98;
-    return (0);
   }
