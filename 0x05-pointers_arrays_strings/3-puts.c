fa2803ac04fcafc3b3d9b1e051240d5e101303c4
Mohammed <mrcloud.med@gmail.com>
2023-10-17 03:04:53 +0200

    str

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * _puts - function that print a string followed by new line.
+  * @str: the stiring to be printed
+  *
+  * Return: nothing
+  */
+void _puts(char *str)
+{
+printf("%s", str);
+}
