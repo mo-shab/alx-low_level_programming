abcf4572507f40816ff9e9b72f526b81424e90b3
Mohammed <mrcloud.med@gmail.com>
2023-10-17 03:11:36 +0200

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * print_rev - function that print a string in reverse
+  * @s: the stiring to be printed
+  *
+  * Return: nothing
+  */
+void print_rev(char *s)
+{
+int len = 0;
+while (s[len] != '\0')
+{
+len++;
+}
+for (i = len; i >= 0; i++)
+{
+printf("%c", s[len]);
+}
+printf("\n");
+}
