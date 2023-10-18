6058695a0e4a51dfe430fe9ca2069f94096c834d
Mohammed <mrcloud.med@gmail.com>
2023-10-19 00:17:04 +0200

    correction

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,6 +3,7 @@
  * _strcat - function appends the src to dest with \0 at the end
  * @src: The char source
  * @dest: the destination of the appends
+ * @var: variable that hold the pointer of dest
  * return: a pointer to the result dest
  */
 char *_strcat(char *dest, char *src)
