87f6a920b9d91f127b49ccd4b21db51eb9046200
Mohammed <mrcloud.med@gmail.com>
2023-10-20 01:10:10 +0200

    rav array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* reverse_array - function to reverse array
+* @a: the array needed to be reversed
+* @n : the number of element of the array
+* Return nothing
+*/
+void reverse_array(int *a, int n)
+{
+int tmp, i;
+
+for (i = 0; i < n/2; i++)
+{
+tmp = a[i];
+a[i] = a[n-1-i];
+a[n-1-i] = tmp;
+}
+}
+
