57b29e9dc526f02e3763ebaa1bf49cc2734d4af0
Mohammed <mrcloud.med@gmail.com>
2023-10-19 02:27:29 +0200

    Correction

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,11 @@
- #include "main.h"
- /**
- * _strcat - function appends the src to dest with \0 at the end
- * @src: The char source
- * @dest: the destination of the appends
- * @var: variable that hold the pointer of dest
- * return: a pointer to the result dest
- */
+#include "main.h"
+/**
+* _strcat - function appends the src to dest with \0 at the end
+* @src: The char source
+* @dest: the destination of the appends
+*
+* Return: a pointer to the result dest
+*/
 char *_strcat(char *dest, char *src)
 {
 char *var = dest;
