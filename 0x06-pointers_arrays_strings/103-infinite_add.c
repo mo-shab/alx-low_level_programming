69bf59fd966cbf5700a27290e5e04aaa2462725d
Mohammed <mrcloud.med@gmail.com>
2023-10-20 17:34:29 +0200

    infinte add

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * infinite_add - adds two integers stored as strings
+ *
+ * @n1: first integer string to add
+ * @n2: second integer string to add
+ * @r: array to store resulting string in
+ * @size_r: size of array r
+ *
+ * Return: the summed string in r. If r is too small for the result,
+ * return 0;
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+int len1 = strlen(n1);
+int len2 = strlen(n2);
+int carry;
+int index_r;
+int digit1, digit2, sum;
+if (len1 + len2 >= size_r)
+{
+return 0;
+}
+carry = 0;
+index_r = 0;
+while (len1 > 0 || len2 > 0 || carry > 0) {
+digit1 = (len1 > 0) ? n1[--len1] - '0' : 0;
+digit2 = (len2 > 0) ? n2[--len2] - '0' : 0;
+int sum = digit1 + digit2 + carry;
+carry = sum / 10;
+r[index_r++] = (sum % 10) + '0';
+}
+r[index_r] = '\0';
+for (int i = 0, j = index_r - 1; i < j; i++, j--)
+{
+char temp = r[i];
+r[i] = r[j];
+r[j] = temp;
+}
+return r;
+}
