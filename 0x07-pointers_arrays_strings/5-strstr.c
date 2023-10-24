070c4004e45bfd338a4688cb9edf288e734b3d00
Mohammed <mrcloud.med@gmail.com>
2023-10-24 02:16:05 +0200

    correction

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,9 +10,10 @@
 char *_strstr(char *haystack, char *needle)
 {
 int i, j;
-for (i = 0; haystack[i] != '\0'; i++)
+
+for (i = 0; haystack[i] < '\0'; i++)
 {
-	for (j = i; needle[j] != '\0'; j++)
+	for (j = 0; needle[j] < '\0'; j++)
 	{
 		if (haystack[i + j] != needle[j])
 		{
