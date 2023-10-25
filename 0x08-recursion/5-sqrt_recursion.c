f5bd3e677dadefa486dfc54776e6e94e11b6b1bc
mo-shab <mrcloud.med@gmail.com>
2023-10-25 23:12:18 +0100

    sqyare

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,19 +4,19 @@
 * @n: the number
 * Return: square root of n
 */
-int sqrt(int n, int j);
+int _sqrt(int n, int j);
 
 int _sqrt_recursion(int n)
 {
 return sqrt(n , 1);
 }
 
-int sqrt(int n, int j)
+int _sqrt(int n, int j)
 {
 int sq = j * j;
 if (sq > n)
 return (-1);
 if (sq == n)
 return (j);
-return (sqrt(n, j + 1));
+return (_sqrt(n, j + 1));
 }
