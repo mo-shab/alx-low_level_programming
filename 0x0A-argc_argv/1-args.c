4276bbfa054e2510afb2d4fd83d7a9a86165c335
mo-shab <mrcloud.med@gmail.com>
2023-10-27 14:03:49 +0100

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - the main function that print number of arg
+* @argc: the number of argument
+* @argv: the arguments
+* Return: 0 if succsess
+*/
+
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
