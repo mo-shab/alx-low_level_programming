4396fc1a1a46a5df08d773429de410d5b90f4ede
mo-shab <mrcloud.med@gmail.com>
2023-10-31 22:57:49 +0100

    Alloc Grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,11 +25,14 @@ int **alloc_grid(int width, int height)
 	{
 		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+			free(grid);
 			return (NULL);
-	}
-	for (i = 0; i < height; i++)
+		}
 		for (j = 0; j < width; j++)
 			grid[i][j] = 0;
-
+	}
 	return (grid);
 }
