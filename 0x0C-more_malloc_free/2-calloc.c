77566dca78aee09efe4d56dd8f6ea0ee406d07a9
mo-shab <mrcloud.med@gmail.com>
2023-11-03 00:00:59 +0100

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,12 +15,15 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *m;
+	unsigned int i = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	m = malloc(nmemb * size);
 	if (m == NULL)
 		return (NULL);
+	while (i < (nmemb * size))
+		m[i++] = 0;
 
 	return (m);
 }
