f784c240a69e937a7fb33dba92cec5ec6b22a686
mo-shab <mrcloud.med@gmail.com>
2023-11-05 17:50:31 +0100

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - structure for dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ */
 struct dog
 {
 	char *name;
