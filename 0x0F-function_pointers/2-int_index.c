91c0ad6be880271af15207646aaa65bf13c27869
Mohammed <mrcloud.med@gmail.com>
2023-11-08 23:17:46 +0100

    adding black line after declaration

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,11 +9,11 @@
  * Return: -1 if no element matches or size <0.
  * The element.
  */
- 
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-	
+
 	if (size <= 0)
 		return (-1);
 	for (i = 0; i < size; i++)
