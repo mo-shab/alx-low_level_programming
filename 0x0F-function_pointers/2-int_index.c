e678c2b3bae8085b6fbbddefaa4df981bdab0202
Mohammed <mrcloud.med@gmail.com>
2023-11-08 23:14:24 +0100

    adding black line after declaration

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Function that return index of an int into an array
+ * @array: The array we will look into
+ * @size: The size of the array
+ * @cmp: function to compare.
+ *
+ * Return: -1 if no element matches or size <0.
+ * The element.
+ */
+ 
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (com(array[i]) == 0)
+			continue;
+		else
+			return (i);
+	}
+	return (-1);
+}
