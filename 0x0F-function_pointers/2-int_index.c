af48760a1954d5c50a8295bc06f718e562ff45dc
Mohammed <mrcloud.med@gmail.com>
2023-11-08 23:24:01 +0100

    adding black line after declaration

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,11 +14,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size > 0 && cmp != NULL)
-		for (i = 0; i < (int)size; i++)
-		{
-			if (cmp(array[i]) != 0)
-				return (i);
-		}
+	if (size <= 0 || cmp == NULL || array == NULL)
+		return (-1);
+	for (i = 0; i < (int)size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+
 	return (-1);
 }
