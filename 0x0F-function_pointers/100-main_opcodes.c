f50aec91d5c9ffe8837e90d96c49c7809e41abe6
Mohammed <mrcloud.med@gmail.com>
2023-11-09 00:43:05 +0100

    OPCODES

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -3,37 +3,40 @@
 #include <string.h>
 
 /**
- * Prints the opcodes of the main function.
+ * main - Prints the opcodes of the main function.
  *
  * @argc: The number of command-line arguments.
  * @argv: An array of strings containing the command-line arguments.
- * @Return: 0 if the program executed successfully, 1 if the number of arguments is incorrect,
+ * @Return: 0 if the program executed successfully,
+ * 1 if the number of arguments is incorrect,
  * 2 if the number of bytes is negative.
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	unsigned char* mainAddress;
-	int numBytes, i;
+	int (*f)(int, char**);
+	int i = 0;
+	int hexByte;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
-		printf("Usage: ./main number_of_bytes\n");
 		exit(1);
 	}
-	numBytes = atoi(argv[1]);
-	if (numBytes < 0)
+	else if (atoi(argv[1]) < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	mainAddress = (unsigned char*) main;
-	printf("Opcode of main function:\n");
-	for (i = 0; i < numBytes; i++)
+	f = main;
+	while (i < atoi(argv[1]))
 	{
-		printf("%02x ", mainAddress[i]);
+		hexByte = *(unsigned char *)(f + i);
+printf("%.2x", hexByte);
+		if (i < atoi(argv[1]) - 1)
+			putchar(' ');
+		i++;
 	}
-	printf("\n");
+	putchar('\n');
 
 	return (0);
 }
