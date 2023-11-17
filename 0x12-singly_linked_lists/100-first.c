41557a540f23508684291d58d39db7da32d5542d
Mohammed <mrcloud.med@gmail.com>
2023-11-17 02:28:07 +0100

    add_end

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,8 @@
  * before_main - function that execute before main
  * Return: Nothing
 */
-void before_main() __attribute__((constructor))
+
+void before_main()
 {
     printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
