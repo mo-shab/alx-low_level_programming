3aa02ae0b87b5a3699900562f23825b1739cc121
Mohammed <mrcloud.med@gmail.com>
2023-11-20 22:40:06 +0100

    linked_list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - Function that print all the elment of a list
+ * @h: the head of the list to be printed
+ * 
+ * Return: number of the list
+*/
+
+size_t print_listint(const listint_t *h)
+{
+    unsigned int n = 0;
+
+    while (h != 0)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        n++;
+    }
+
+    return (n);
+}
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * listint_len - function that returns the
+ * number of elements in a linked listint_t list.
+ * @h: The head of the list
+ * 
+ * Return: Lenght of the list
+*/
+
+size_t listint_len(const listint_t *h)
+{
+    unsigned int len = 0;
+
+    while (h != NULL)
+    {
+        h = h->next;
+        len++;
+    }
+
+    return (len);
+}
