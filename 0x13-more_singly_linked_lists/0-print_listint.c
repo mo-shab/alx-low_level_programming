2a61c7a05fa0e89f28760f95bfdab49a34e4fca3
Mohammed <mrcloud.med@gmail.com>
2023-11-20 22:43:19 +0100

    betty_style

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,20 +3,20 @@
 /**
  * print_listint - Function that print all the elment of a list
  * @h: the head of the list to be printed
- * 
+ *
  * Return: number of the list
 */
 
 size_t print_listint(const listint_t *h)
 {
-    unsigned int n = 0;
+	unsigned int n = 0;
 
-    while (h != 0)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        n++;
-    }
+	while (h != 0)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		n++;
+	}
 
-    return (n);
+	return (n);
 }
