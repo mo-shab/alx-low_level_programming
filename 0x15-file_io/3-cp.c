ca7bcc73b81aa2919240b96f26969608355e73fe
Mohammed <mrcloud.med@gmail.com>
2023-11-28 01:33:26 +0100

    Files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,16 +14,16 @@ int main(int argc, char *argv[])
 	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
-		print_error_and_exit(97, "Usage: %s file_from file_to\n", argv[0]);
+		print_err_exit(97, "Usage: %s file_from file_to\n", argv[0]);
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-		print_error_and_exit(98, "Error: Can't read from file %s\n", argv[1]);
+		print_err_exit(98, "Error: Can't read from file %s\n", argv[1]);
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR |
 				S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (file_to == -1)
 	{
 		close(file_from);
-		print_error_and_exit(99, "Error: Can't write to file %s\n", argv[2]);
+		print_err_exit(99, "Error: Can't write to file %s\n", argv[2]);
 	}
 	while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
 	{
@@ -32,19 +32,19 @@ int main(int argc, char *argv[])
 		{
 			close(file_from);
 			close(file_to);
-			print_error_and_exit(99, "Error: Can't write to file %s\n", argv[2]);
+			print_err_exit(99, "Error: Can't write to file %s\n", argv[2]);
 		}
 	}
 	if (bytes_read == -1)
 	{
 		close(file_from);
 		close(file_to);
-		print_error_and_exit(98, "Error: Can't read from file %s\n", argv[1]);
+		print_err_exit(98, "Error: Can't read from file %s\n", argv[1]);
 	}
 	if (close(file_from) == -1)
-		print_error_and_exit(100, "Error: Can't close fd %d\n", file_from);
+		print_err_exit(100, "Error: Can't close fd %d\n", argv[1]);
 	if (close(file_to) == -1)
-		print_error_and_exit(100, "Error: Can't close fd %d\n", file_to);
+		print_err_exit(100, "Error: Can't close fd %d\n", argv[0]);
 
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,4 +14,5 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
+void print_err_exit(int code, const char *msg, const char *filename);
 #endif
