d7f421aaf2b4132e812bbc55bf12753b262f5fb1
Mohammed <mrcloud.med@gmail.com>
2024-01-10 12:23:05 +0000

    task1

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,13 +2,13 @@
 
 /**
 * print_dlistint - Function that print all element on a list
-* @dlistint_t: the list to be printed
+* @h: the list to be printed
 * Return: number of nodes
 */
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	int n = 0;
+	size_t n = 0;
 
 	while (h != NULL)
 	{
---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include <lists.h>
+
+/**
+* dlistint_len - function that returns the number of elements in a linked list
+* @h: The list
+* Return: length of the list
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+
+	return (len);
+}
