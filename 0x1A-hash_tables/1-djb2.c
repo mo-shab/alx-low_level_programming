95aabbeda93dac3f67ab49d42cf8452934aa1960
Mohammed <mrcloud.med@gmail.com>
2024-02-02 23:40:12 +0000

    djb2

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - convert a str into a hash value.
+ * @str: string to convert
+ *
+ * Return: Hash (unsigned long int)
+ */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
     unsigned long int hash;
