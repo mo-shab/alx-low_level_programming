15ccc91903fa61bd50f5b68f603c27d504a75aee
Mohammed <mrcloud.med@gmail.com>
2024-02-03 00:28:57 +0000

    print_hash_table

--- 0x1A-hash_tables/4-main.c
+++ 0x1A-hash_tables/4-main.c
@@ -13,19 +13,19 @@ int main(void)
     hash_table_t *ht;
     char *value;
 
-    ht = hash_table_create(1024);
-    hash_table_set(ht, "c", "fun");
+    ht = hash_table_create(10);
+    /*hash_table_set(ht, "c", "fun");*/
     hash_table_set(ht, "python", "awesome");
-    hash_table_set(ht, "Bob", "and Kris love asm");
+    /*hash_table_set(ht, "Bob", "and Kris love asm");
     hash_table_set(ht, "N", "queens");
     hash_table_set(ht, "Asterix", "Obelix");
     hash_table_set(ht, "Betty", "Cool");
     hash_table_set(ht, "98", "Battery Street");
-    hash_table_set(ht, "c", "isfun");
+    hash_table_set(ht, "c", "isfun");*/
 
     value = hash_table_get(ht, "python");
     printf("%s:%s\n", "python", value);
-    value = hash_table_get(ht, "Bob");
+    /*value = hash_table_get(ht, "Bob");
     printf("%s:%s\n", "Bob", value);
     value = hash_table_get(ht, "N");
     printf("%s:%s\n", "N", value);
@@ -38,6 +38,6 @@ int main(void)
     value = hash_table_get(ht, "c");
     printf("%s:%s\n", "c", value);
     value = hash_table_get(ht, "javascript");
-    printf("%s:%s\n", "javascript", value);
+    printf("%s:%s\n", "javascript", value);*/
     return (EXIT_SUCCESS);
 }
