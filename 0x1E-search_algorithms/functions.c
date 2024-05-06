05c4c3e46fc17726a879220ba25be8fb8031c09f
Mohammed <mrcloud.med@gmail.com>
2024-05-06 17:39:55 +0200

    void put

--- 0x1E-search_algorithms/functions.c
+++ 0x1E-search_algorithms/functions.c
@@ -1,3 +1,21 @@
 #include "search_algos.h"
 
-_put(string str);
+/**
+ * _put - function that prints a string
+ * 
+ * @str: string to print
+ * 
+ * Return: void
+*/
+
+void _put(char *str)
+{
+    if (str == NULL)
+        return;
+
+    while (*str)
+    {
+        putchar(*str);
+        str++;
+    }
+}
