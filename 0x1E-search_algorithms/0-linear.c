e7bf9d8143264c2736ab39d2244f85900bbbc1e3
Mohammed <mrcloud.med@gmail.com>
2024-05-07 11:12:37 +0100

    BEtty Style

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,11 +2,11 @@
 
 /**
  * linear_search - function that searches for a value in an array of integers
- * 
+ *
  * @array: pointer to the first element of the array to search in
  * @size: number of elements in array
  * @value: value to search for
- * 
+ *
  * Return: the first index where value is located
 */
 
